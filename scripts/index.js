6253cfa6488dccb2279ac8367fcc854db5ad79d4
Ksenia Gurenko <gurenochka@inbox.ru>
2024-08-30 15:25:39 +0300

    Iteration_1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content; 
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(card, deleteCard) {
+   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+   const deleteButton = cardElement.querySelector('.card__delete-button');
+   cardElement.querySelector('.card__image').src = card.link;
+   cardElement.querySelector('.card__title').textContent = card.name;
+   deleteButton.addEventListener('click', deleteCard);
+   return cardElement;
+}
 
 // @todo: Функция удаления карточки
-
+ function deleteCard(event) {
+    event.target.closest('.card').remove()
+ }
+ 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(card) {
+    let Element = createCard(card, deleteCard);
+    placesList.append(Element);
+});
+ 
